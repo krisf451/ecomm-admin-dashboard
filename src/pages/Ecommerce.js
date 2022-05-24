1cfe111aa96ffe5066f69b257dc4d10972471706
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-24 16:16:33 -0700

    feat: setup ecommerce page and import icons, context, dummy data, and the components we will use

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { BsCurrenceDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
   return <div>Ecommerce</div>;
