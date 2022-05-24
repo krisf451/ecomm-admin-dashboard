b620a85ebedbbeabd7c9705873799614eaeb76bc
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-24 14:45:25 -0700

    feat: added all nav icons in reusuable components, user,chat,cart,menu,notifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,6 +29,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
+
+  const handleClick = (btn) => {};
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
@@ -37,6 +39,43 @@ const Navbar = () => {
         color="blue"
         icon={<AiOutlineMenu />}
       />
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03c9d7"
+          customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03c9d7"
+          customFunc={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center cursor-pointer gap-2 p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("user")}
+          >
+            <img src={avatar} alt="user" className="rounded-full w-8 h-8" />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
     </div>
   );
 };
