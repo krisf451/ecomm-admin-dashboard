5fae23e3e653014e563d4fa2ad168eadfd7fc6db
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-25 18:44:50 -0700

    feat: calendar done, attached dummy data to simulate events that are sorted by time, syncfusion has a lot out of the box including drag and drop, and CRUD functionallity, will eventually hook this up to a backend

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -13,12 +13,22 @@ import {
   DragAndDrop,
 } from "@syncfusion/ej2-react-schedule";
 import { DataPickerComponent } from "@syncfusion/ej2-react-calendars";
+import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
 
 const Calendar = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-xl">
-      Calendar
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
     </div>
   );
 };
