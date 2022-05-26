cbcf02409ee68b4c4b762d76af42cfb6b241f146
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-25 22:00:51 -0700

    FIX: theme sidebar darkmode - FEAT: Linechart Page

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  LineSeries,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "white"}
+    >
+      <Inject services={[LineSeries, Tooltip, Legend, DateTime]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, i) => (
+          <SeriesDirective key={i} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
