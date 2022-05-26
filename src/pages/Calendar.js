fe90704f330159013c01067dffdd1b4f5312d49a
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-25 18:36:44 -0700

    fix: styling issue with margin on the header, switched back to having the top margin on the pages

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DataPickerComponent } from "@syncfusion/ej2-react-calendars";
+import { Header } from "../components";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-xl">
+      Calendar
+    </div>
+  );
 };
 
 export default Calendar;
